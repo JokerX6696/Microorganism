a4509ffde9bb8c3af843adbadabacc51bc89951b
Fuxing Zheng <106021349+JokerX6696@users.noreply.github.com>
2023-04-25 14:21:31 +0800

    为显著相关的格子添加 ‘*’

--- Bacterial_correlation_plot_v3.r
+++ Bacterial_correlation_plot_v3.r
@@ -2,29 +2,31 @@ rm(list=ls())
 setwd('D:/desk/DZQD2023040574-b1/20221118_QD2021-19862_韦超老师的12个小鼠角膜组织2bRAD-M微生物菌群检测项目_第2次个性化_结题报告/report/fig3')
 library(reshape2)
 library(pheatmap)
-file <- 'genus_species_correlation.xls'
+file <- 'species_correlation.xls'
 df <- read.table(file,header = TRUE, sep = '\t')
 
-df_c <- df[,-4]
-df_c <- dcast(data = df, formula =  Category1 ~ Category2 )
-
+df_c <- df[,-3]
+df_pvalue <- df[,-4]
+df_c <- dcast(data = df_c, formula =  Category1 ~ Category2 )
+df_pvalue <- dcast(data = df_pvalue, formula =  Category1 ~ Category2 )
 rownames(df_c) <- df_c$Category1
-
+rownames(df_pvalue) <- df_c$Category1
 df_c <- df_c[,-1]
-
+df_pvalue <- df_pvalue[,-1]
 p <- pheatmap(df_c)
 dev.off()
 df_p <- df_c[p$tree_row$order,p$tree_col$order,]
-
+df_pvalue <- df_pvalue[p$tree_row$order,p$tree_col$order,]
 
 s = c()
-for(i in as.matrix(df_p)){
-  print(i)
-  if(i >= 0.9){
+for(w in as.matrix(df_pvalue)){
+  #print(i)
+  i <- as.numeric(w)
+  if(i <= 0.001){
     s <- c(s,"***")
-  }else if(i >= 0.8){
+  }else if(i <= 0.01){
     s <- c(s,"**")
-  }else if(i >= 0.6){
+  }else if(i <= 0.05){
     s <- c(s,"*")
   }else{
     s <- c(s," ")
