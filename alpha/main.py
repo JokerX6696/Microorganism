a3d5114642f7c6df22276f1187eda76c5b66553e
JokerX6696 <313445042@qq.com>
2023-08-28 16:14:38 +0800

    还差 ACE 值,标签基本不会出现为 1的 情况

--- alpha/main.py
+++ alpha/main.py
@@ -3,8 +3,9 @@ import pandas as pd
 from method import Chao1, ACE
 # para
 host = 'human'
+enzyme = 'BcgI'
 # 获取矩阵
-df = get_tag_matrix(wd='D:/desk/python_wkdir/Quan')
+df = get_tag_matrix(wd='D:/desk/python_wkdir/Quan',jk=enzyme)
 # 删除宿主
 df = df[df.index != host]
 # 计算各个样本 alpha 多样性
@@ -13,10 +14,11 @@ alpha_dict = {}
 for sample in samples:
     alpha_dict[sample] = {}
     alpha_dict[sample]['observe'], alpha_dict[sample]['chao1'] = Chao1(df[sample])
-    #alpha_dict[sample]['ACE'] = ACE(df[sample])
+    alpha_dict[sample]['ACE'] = ACE(df[sample])
 
 
-df_alpha = pd.DataFrame(alpha_dict)
+df_alpha = pd.DataFrame(alpha_dict).T
+df_alpha['observe'] = df_alpha['observe'].astype(int)  # observe 一定为整数 防止歧义
 
 
 
