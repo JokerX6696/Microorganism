c23d88af295770e3e5b4061a81d35aa56624898a
JokerX6696 <313445042@qq.com>
2023-08-29 15:53:31 +0800

    封装完成,但ACE值小于observe

--- alpha/main.py
+++ alpha/main.py
@@ -14,15 +14,11 @@ alpha_dict = {}
 for sample in samples:
     alpha_dict[sample] = {}
     alpha_dict[sample]['observe'], alpha_dict[sample]['chao1'] = Chao1(df[sample])
-    alpha_dict[sample]['ACE'] = ACE(df[sample])
+    alpha_dict[sample]['ACE'] = ACE(df[sample],rare_tag_threshold=3)
 
 
 df_alpha = pd.DataFrame(alpha_dict).T
 df_alpha['observe'] = df_alpha['observe'].astype(int)  # observe 一定为整数 防止歧义
-
-
-
-
 
 
 print(df_alpha)
--- alpha/method.py
+++ alpha/method.py
@@ -1,3 +1,14 @@
+def stat_list(lst):  # 统计列表频次
+    stat_dict = {}
+    for i in lst:
+        if i in stat_dict:
+            stat_dict[i] += 1
+        else:
+            stat_dict[i] = 1
+    return stat_dict
+
+
+
 def Chao1(value_list):  # 顺便返回 S_obs
     S_obs = sum(1 for q in value_list if q != 0)
     F1 = sum(1 for q in value_list if q == 1)
@@ -8,6 +19,24 @@ def Chao1(value_list):  # 顺便返回 S_obs
 
 def ACE(value_list, rare_tag_threshold=10):
     num_rare = sum(1 for q in value_list if q <= rare_tag_threshold and q > 0) #  默认 小于 10个 标签的 认为是 稀有物种
+    
+    # 计算稀有物种 标签数 小于 10 的 物种 Σ i*Fi
+    n_rare = 0
+    stat_num = stat_list(value_list)
+    s_rare = 0 # 稀有物种的物种数
+    F_i_sum = 0 # 计算 gamma_ace 用到的参数
+    s_high_abd = 0 # 高丰度物种 标签数大于阈值的物种
+    for i in stat_num:  # 这里在字典的 key 中循环 也就是 unique 后的 tag 数量
+        if i <= rare_tag_threshold and i != 0:
+            n_rare += (i * stat_num[i])
+            s_rare += 1
+            F_i_sum += (i*(i-1)*stat_num[i])
+        elif i > rare_tag_threshold:
+            s_high_abd += stat_num[i]
     F1 = sum(1 for q in value_list if q == 1) # F1 为 仅有一个标签的物种
-    cover_ace = 1 - F1/num_rare   # cover_ace 为样本覆盖度的估计值
-    return 0
+    print(n_rare)
+    cover_ace = 1 - (F1/n_rare)   # cover_ace 为样本覆盖度的估计值
+    gamma_ace = max([(s_rare * F_i_sum)/(cover_ace*n_rare*(n_rare-1)), 0])  # 稀有物种的变异系数
+    s_ace = s_high_abd + s_rare/cover_ace + F1*gamma_ace/cover_ace
+    
+    return s_ace
