98157764a7f3f25116a0796e6efc1f056116dc22
JokerX6696 <313445042@qq.com>
2023-12-18 14:06:31 +0800

    修改为3样本

--- bar_plot_point_errorbar.r
+++ bar_plot_point_errorbar.r
@@ -1,5 +1,5 @@
 rm(list=ls())
-setwd('D:/desk/XMSH_202311_7584/2.alpha_beta')
+setwd('D:/desk/XMSH_202312_8327/2.alpha_beta')
 library('ggplot2')
 library('ggpubr')
 
@@ -10,10 +10,10 @@ g <- gsub("_.*","",df$Sample)
 df$Group <- g
 df <- df[,c("Sample","Group",'Shannon')]
 
-avg <- c(mean(df$Shannon[1:3]),mean(df$Shannon[4:6]),mean(df$Shannon[7:9]),mean(df$Shannon[10:12]))
-errbar = c(sd(df$Shannon[1:3]),sd(df$Shannon[4:6]),sd(df$Shannon[7:9]),sd(df$Shannon[10:12]))
+avg <- c(mean(df$Shannon[1:3]),mean(df$Shannon[4:6]),mean(df$Shannon[7:9]))
+errbar = c(sd(df$Shannon[1:3]),sd(df$Shannon[4:6]),sd(df$Shannon[7:9]))
 
-df_avg <- data.frame(Group=unique(g),avg=c(mean(df$Shannon[1:3]),mean(df$Shannon[4:6]),mean(df$Shannon[7:9]),mean(df$Shannon[10:12])))
+df_avg <- data.frame(Group=unique(g),avg=c(mean(df$Shannon[1:3]),mean(df$Shannon[4:6]),mean(df$Shannon[7:9])))
 df_avg$err <- errbar
 
 df_avg$Group <- factor(df_avg$Group,levels = df_avg$Group)
@@ -24,17 +24,17 @@ ret <- aov(value ~ Group,df_aov)
 ret <- anova(ret)$"Pr(>F)"[1]
 ret <- format(ret, scientific = TRUE)
 ret <- signif(as.numeric(ret), digits = 2)
-pvalue <- paste0('** P = ',ret)
+pvalue <- paste0('* P = ',ret)
 p <- ggplot() + 
   geom_bar(data = df_avg,mapping = aes(x=Group,y=avg,fill=Group),stat = 'identity',width = 0.6,alpha = 0.7) + 
   geom_point(data = df,mapping = aes(x=Group,y=Shannon,fill=Group), size = 2,show.legend = F) +
   theme_classic() + 
   geom_errorbar(data = df_avg,aes( ymin=(avg-err),ymax=(avg+err),x=Group),width = 0.2,stat = "identity",position = "identity") + 
-  scale_fill_manual(values = c('#00468BFF','#ED0000FF','#42B540FF','#0099B4FF')) + 
+  scale_fill_manual(values = c('#00468BFF','#ED0000FF','#42B540FF')) + 
   labs(y='Shannon') + 
   scale_y_continuous(limits = c(0,4),expand = c(0,0)) + 
   geom_signif(data=df_avg,
-              aes(xmin='blank', xmax='A5B5', annotations=pvalue, y_position=3.5),
+              aes(xmin='PBS', xmax='A5B5', annotations=pvalue, y_position=3.5),
               textsize = 6, vjust = 0.05, tip_length = c(0.02, 0.02),
               manual=TRUE) 
   
